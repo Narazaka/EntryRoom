2e132a00c124930ed84670cd69a9286eb2119b12
Narazaka <info@narazaka.net>
2025-06-29 20:35:26 +0900

    del fadeteleport ref

--- Udon/Helper/EntryRoomHelper.cs
+++ Udon/Helper/EntryRoomHelper.cs
@@ -1,13 +1,11 @@
 using UnityEngine;
 using VRC.SDK3.Components;
-using Narazaka.VRChat.FadeTeleport;
 
 namespace Narazaka.VRChat.EntryRoom.Helper
 {
     public class EntryRoomHelper : MonoBehaviour
     {
         public VRCSceneDescriptor vrcWorld;
-        public FadeTeleportTo FadeTeleportTo;
         public Transform gimmickRoot;
         public Transform parent;
     }
